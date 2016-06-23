37b71e569343a86208f3b9e6747a6e2ac854bfb7
Bosse Edström <bosse.edstrom@gmail.com>
2016-06-23 09:36:05 +0200

    Added [Authorize]

--- LMS/Controllers/CoursesController.cs
+++ LMS/Controllers/CoursesController.cs
@@ -43,8 +43,6 @@ namespace LMS.Controllers
         }
 
         // POST: Courses/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Name,Description,StartDate,EndDate")] Course course)
@@ -75,8 +73,6 @@ namespace LMS.Controllers
         }
 
         // POST: Courses/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Name,Description,StartDate,EndDate")] Course course)
