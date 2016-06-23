cf4576108c9bdcd2afa4b578565063692e498145
andreasc <carsbring@gmail.com>
2016-06-23 09:39:33 +0200

    Modules Controller added

--- LMS/Models/IdentityModels.cs
+++ LMS/Models/IdentityModels.cs
@@ -33,5 +33,7 @@ namespace LMS.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<LMS.Models.Module> Modules { get; set; }
     }
 }
